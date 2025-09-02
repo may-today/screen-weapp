abc2e8c1e08d23b36f022dcd993a36fe43b4a456
ddiu8081 <ddiu8081@gmail.com>
2025-09-03 02:14:37 +0800

    feat: message store

--- src/stores/message.ts
+++ src/stores/message.ts
@@ -1,4 +1,24 @@
 import { makeAutoObservable } from 'mobx-miniprogram'
+import type { Command } from '@/types'
+
+type MessageData = {
+  /** 消息持续时间 */
+  duration: number
+  /** 消息指令 */
+  command: Command
+  /** 消息附加数据 */
+  payload?: string | number
+}
+
+type Message = {
+  meta: {
+    /** 消息时间戳 */
+    timestamp: number
+    /** 消息等级 */
+    level: 'debug' | 'info' | 'error'
+  }
+  data: MessageData
+}
 
 // 消息种类：
 // 1. 连接状态消息（连接授权、连接成功、连接中、连接断开）
@@ -6,10 +26,20 @@ import { makeAutoObservable } from 'mobx-miniprogram'
 // 3. 长数据消息（歌词文本、自定义文字）
 
 export class MessageStore {
-  messageList = [] as string[]
+  messageList = [] as Message[]
 
   constructor() {
     makeAutoObservable(this)
+  }
+  
+  addMessage(data: MessageData, level: 'debug' | 'info' | 'error' = 'info') {
+    this.messageList.push({
+      meta: {
+        timestamp: Date.now(),
+        level,
+      },
+      data,
+    })
   }
 
   // setConnectStatus(connectStatus: ConnectStatus) {
